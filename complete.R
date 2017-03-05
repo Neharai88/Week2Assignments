01bc8f878a2c0e361c5aafce89f3bb9b33596f27
Neha Rai <neharai2488@gmail.com>
2017-03-05 21:18:21 +0530

    adding week2assignments

---
+++ complete.R
@@ -0,0 +1,8 @@
+complete <- function(directory,id = 1:332) {
+#https://rpubs.com/SatoshiLiang/16516
+     nobs <- function(id) {
+        path <- file.path(directory, paste(sprintf("%03d", as.numeric(id)), ".csv", sep=""))
+        return (sum(complete.cases(read.csv(path))))
+    }
+return (data.frame(id=id, nobs=sapply(id, nobs)))
+}
